cce1bf89453a4741a979c8ca0973a43c146d7b2d
Kamonlak <Rthkad2542@gmail.com>
2019-10-06 11:29:10 +0700

    lab6

---
+++ src/lab7/ex1.java
@@ -0,0 +1,21 @@
+package lab7;
+
+    public class ex1 {
+        public static void main(String[] args) {
+
+            int x = 100, y = 20;
+
+            //if
+            if (x < y && x==100 || y>10) {
+
+                //statemets
+                System.out.println("x less than y.");
+                System.out.println("Bye.");
+            }else {
+                System.out.println("x more then y.");
+            }
+
+        }//main
+
+    }//class
+
