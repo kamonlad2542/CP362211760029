9de6cd1b0c16e48e55a7b7555dcdf2799dc37635
Kamonlak <Rthkad2542@gmail.com>
2019-10-06 11:07:45 +0700

    lab6

--- src/lab6/lad6_2.java
+++ src/lab6/lad6_2.java
@@ -1,19 +1,17 @@
 package lab6;
 
-public class lad6_2 {
 import java.util.Scanner;
 
-    public class lab_2 {
-        public static void  main (String[] args) {
-            Scanner sc =new Scanner(System.in);
-            double i;
-            System.out.println("Input length in in inch;");
-            i = sc.nextDouble();
-            double m = i*0.0254;
-            System.out.println(i+ "inch is egual to "+m+" meters.");
+public class lad6_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double i;
+        System.out.print("Input length in inch");
+        i = scanner.nextDouble();
+        double m = i * 0.0254;
 
-        }
+        System.out.print(i + "inch is equal to " + m + " meters. ");
+
 
     }
-
 }
--- src/lab6/lad6_3.java
+++ src/lab6/lad6_3.java
@@ -1,9 +1,21 @@
 package lab6;
 
-public class lad6_3 {
+import java.util.Scanner;
 
+public class lad6_3{
+    public static void main(String[] args) {
+        {
+            //BMT = weight/(hight*hight)
+            Scanner in = new Scanner(System.in);
 
+            System.out.print("weight:");
+            float weight = in.nextFloat();
 
+            System.out.print("hight:");
+            float hight = in.nextFloat();
 
-
+            float bmi = weight / (hight * hight);
+            System.out.print(bmi);
+        }
+    }
 }
